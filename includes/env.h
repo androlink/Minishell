970d9c1676cd9a7e9a505cd0856382f15f7999aa
gcros <guilhem.cros.pro@gmail.com>
2024-04-04 03:23:09 +0200

    env base todo

--- includes/env.h
+++ includes/env.h
@@ -6,17 +6,22 @@
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 04:44:12 by gcros             #+#    #+#             */
-/*   Updated: 2024/03/21 04:46:35 by gcros            ###   ########.fr       */
+/*   Updated: 2024/04/04 01:24:31 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
 
+#include "list.h"
+
 typedef struct s_env
 {
-	
+	char	*tok;
+	t_env	*left;
+	t_env	*right;
 }	t_env;
+
 
 
 #endif
