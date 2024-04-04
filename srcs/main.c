051bab5bbfe4d5692d5fb016bb78b2b4ebc11862
wartro <contact@wartro.fr>
2024-04-04 19:56:41 +0200

    add prompt branch

--- srcs/main.c
+++ srcs/main.c
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
+/*   By: wartro <wartro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 04:37:28 by gcros             #+#    #+#             */
-/*   Updated: 2024/03/21 05:22:41 by gcros            ###   ########.fr       */
+/*   Updated: 2024/04/04 19:55:17 by wartro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <readline/readline.h>
 #include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 
 int	main(int ac, char **av, char **env)
 {
