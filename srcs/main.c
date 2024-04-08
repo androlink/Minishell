5776c7d71376273ef9713ca36f5a4763592dfc2a
gcros <guilhem.cros.pro@gmail.com>
2024-04-08 17:43:25 +0200

    .

--- srcs/main.c
+++ srcs/main.c
@@ -6,25 +6,29 @@
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 04:37:28 by gcros             #+#    #+#             */
-/*   Updated: 2024/03/21 05:22:41 by gcros            ###   ########.fr       */
+/*   Updated: 2024/04/06 02:45:51 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <readline/readline.h>
+#include "env.h"
+#include "str.h"
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main(int ac, char **av, char **env)
 {
 	(void) ac;
 	(void) av;
 	(void) env;
-	char **s;
 
-	s = env;
-	while (*s != NULL)
-	{
-		printf("%s\n", *s);
-		s++;
-	}
+	t_env	*e;
+
+	e = NULL;
+	printf("%p", *env);
+	printf("%d\n", ms_env_gen(env, &e));
+	printf("%s\n", ms_env_get(e, "test"));
+	ms_env_collapse(&e);
+
 	return (0);
 }
