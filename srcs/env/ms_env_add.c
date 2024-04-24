21564e7ccecfc317d97b96223cac4b91a02ec2bf
gcros <guilhem.cros.pro@gmail.com>
2024-04-24 05:10:57 +0200

    chang and add function

--- srcs/env/ms_env_add.c
+++ srcs/env/ms_env_add.c
@@ -6,7 +6,7 @@
 /*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 03:16:23 by gcros             #+#    #+#             */
-/*   Updated: 2024/04/10 05:05:41 by gcros            ###   ########.fr       */
+/*   Updated: 2024/04/23 22:12:27 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,10 @@ int	ms_env_add(t_env **head, t_env *e)
 	cmp = ft_strncmp((*head)->key, e->key, ft_strlen(e->key) + 1);
 	if (cmp == 0)
 	{
-		if (e->value != NULL)
-		{
-			free((*head)->value);
-			(*head)->value = e->value;
-			free(e->key);
-			free(e);
-		}
+		free((*head)->value);
+		(*head)->value = e->value;
+		free(e->key);
+		free(e);
 		return (0);
 	}
 	else if (cmp < 0)
