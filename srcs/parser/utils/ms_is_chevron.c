987e76e129bc11834dd158d584f55d2c89a2a82e
wartro <contact@wartro.fr>
2024-05-20 15:45:29 +0200

    [NORM] PART parser/utils

--- srcs/parser/utils/ms_is_chevron.c
+++ srcs/parser/utils/ms_is_chevron.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_is_chevron.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmorot <mmorot@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 15:44:23 by mmorot            #+#    #+#             */
+/*   Updated: 2024/05/20 15:44:29 by mmorot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "conf.h"
 #include "parser.h"
 
-int ms_is_chevron(t_type type)
+int	ms_is_chevron(t_type type)
 {
 	if (type == E_REDIR_IN
 		|| type == E_REDIR_OUT
 		|| type == E_APPEND
 		|| type == E_HEREDOC)
 		return (1);
-    return (0);
+	return (0);
 }
--- srcs/parser/utils/ms_reset_quote.c
+++ srcs/parser/utils/ms_reset_quote.c
@@ -1,11 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_reset_quote.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmorot <mmorot@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 15:44:48 by mmorot            #+#    #+#             */
+/*   Updated: 2024/05/20 15:44:58 by mmorot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parser.h"
 
-int ms_reset_quote(t_prompt_s *status)
+int	ms_reset_quote(t_prompt_s *status)
 {
-	// printf("quote-A\n");
 	if (status->squote + status->dquote > 1)
 	{
-		// printf("quote-B\n");
 		status->squote = 0;
 		status->dquote = 0;
 		return (1);
