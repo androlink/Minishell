c2620e1010dbf0b6a798f089705d497cc005890e
gcros <guilhem.cros.38@gmail.com>
2024-06-05 19:29:06 +0200

    error rework

--- srcs/parser/syntax/ms_syntax_error.c
+++ srcs/parser/syntax/ms_syntax_error.c
@@ -3,36 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   ms_syntax_error.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmorot <mmorot@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: gcros <gcros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 15:29:02 by mmorot            #+#    #+#             */
-/*   Updated: 2024/05/23 21:58:19 by mmorot           ###   ########.fr       */
+/*   Updated: 2024/06/05 19:27:58 by gcros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "conf.h"
 #include "parser.h"
+#include "put.h"
 
 int	ms_syntax_error(t_error error, char *msg, t_shell *shell)
 {
-	if (!shell->prompt_listen)
+	char	*str;
+
+	if (shell->prompt_listen == 0)
 		return (0);
+	ms_set_status(2 << 8);
+	shell->prompt_listen = 0;
+	str = NULL;
 	if (error == E_SYNTAX_UPD_TOK)
 	{
 		if (!msg)
-		{
 			shell->error = 1;
-			shell->prompt_listen = 0;
+		if (!msg)
 			return (0);
-		}
-		printf(MS_NAME ERR_SYNTAX_TOK, msg);
+		str = ft_strsjoin((char *[])
+			{"Mishell : syntax error near unexpected token `", msg, "`", NULL});
 	}
 	else if (error == E_SYNTAX_UPD_EOF)
-		printf(MS_NAME ERR_SYNTAX_UPD_EOF);
+		str = ft_strdup("Mishell : syntax error: unexpected `end of file'");
 	else if (error == E_SYNTAX_UPD_NLN)
-		printf(MS_NAME ERR_SYNTAX_UPD_NLN);
-	if (!msg)
-		free(msg);
-	shell->prompt_listen = 0;
+		str = ft_strdup("Mishell : syntax error: unexpected `newline'");
+	free(msg);
+	if (str)
+		ft_putendl_fd(str, 2);
+	free(str);
 	return (0);
 }
